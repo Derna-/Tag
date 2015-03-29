288d2e499e3d41fee1089e15c595da9b1aa1a0b8
Derna <nader.georgi@gmail.com>
2015-03-29 21:34:55 +0200

    Choisir une image depuis la mémoire. Afficher l'image en mode plein ecran Appliquer SIFT

--- app/src/main/java/teamone/tag/ViewImage.java
+++ app/src/main/java/teamone/tag/ViewImage.java
@@ -203,17 +203,4 @@ public class ViewImage extends ActionBarActivity {
         }
     };
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        if(id == android.R.id.home) {
-            NavUtils.navigateUpFromSameTask(this);
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
